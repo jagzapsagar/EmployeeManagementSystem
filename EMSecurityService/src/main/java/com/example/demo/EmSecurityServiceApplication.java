7799c22cb08ed0eab8c12769279374a3212c30a9
StoryOnJava <jagzapsagar228@gmail.com>
2025-03-31 15:54:03 +0530

    Security service added working on it -> NOT COMPLETED

---
+++ EMSecurityService/src/main/java/com/example/demo/EmSecurityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmSecurityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmSecurityServiceApplication.class, args);
+	}
+
+}
---
+++ EMSecurityService/src/main/java/com/example/demo/config/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.example.demo.config;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.example.demo.entity.User;
+import com.example.demo.repo.UserRepo;
+
+public class CustomUserDetailsService implements UserDetailsService {
+	
+	 @Autowired
+	 private UserRepo repository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		Optional<User> credential = repository.findByName(username);
+        return credential.map(CustomUserDetails::new).orElseThrow(() -> 
+        new UsernameNotFoundException("user not found with name :" + username));
+		//return null;
+	}
+
+}
